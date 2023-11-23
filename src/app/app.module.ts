bee44affb9be0ac2a035aefd13e23d7e910b8b24
Harsán Péter <hapegabor@gmail.com>
2023-11-23 15:57:18 +0100

    Run as PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
 import { MatButtonModule } from '@angular/material/button';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment.prod';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,9 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     AppRoutingModule,
     MatButtonModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
+//      enabled: !isDevMode(),
+      enabled: environment.production,
+      // enabled: environment.production // in the sample
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
