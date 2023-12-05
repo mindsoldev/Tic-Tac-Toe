b0873d79200f7e3851e34165fbaa677b73567482
Harsán Péter <pharsan@yahoo.com>
2023-12-05 17:41:47 +0100

    iPhone disabled button style corrected

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -5,13 +5,13 @@ type SquareChar = 'X' | 'O';
 @Component({
   selector: 'app-square',
   template: `
-      <button mat-disabled-button *ngIf="!value">{{ value }}</button>
+      <button mat-button color="disabled" *ngIf="!value">{{ value }}</button>
       <button mat-raised-button color="primary" *ngIf="value == 'X'">{{ value }}</button>
       <button mat-raised-button color="accent" *ngIf="value == 'O'">{{ value }}</button>
   `,
   styles: [
-      'button { width: 100%; height: 100%; font-size: 5em !important; }',
-      '.mat-disabled { background-color: green; color: #fff;}'
+      'button { width: 100%; height: 100%; font-size: 5em !important;}',
+      '.mat-disabled { background-color: #dbdbdb; color: #fff;}'
     ]
 })
 export class SquareComponent {
